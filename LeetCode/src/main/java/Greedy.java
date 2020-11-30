c829fdb92fc7dec26975f19ef497474317488910
6ylqq <937192804@qq.com>
2020-12-01 01:02:52 +0800

    贪心算法。与动态规划的区别：贪心算法的每一步的最优都和下一步最优关联，所以不需要和动态规划那样子记录每一步的最优解然后比较得出当前最优解

---
+++ LeetCode/src/main/java/Greedy.java
@@ -0,0 +1,66 @@
+class MaxProfit {
+    /**
+     * 一次股票交易包含买入和卖出，只进行一次交易，求最大收益。
+     * */
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 0) {
+            return 0;
+        }
+        int minPrice = prices[0];
+        int maxProfit = 0;
+            for (int price : prices) {
+                    if (price < minPrice) {
+                            minPrice = price;
+                    } else {
+                            maxProfit = Math.max(maxProfit, price - minPrice);
+                    }
+            }
+        return maxProfit;
+    }
+
+    /**
+     * 可以进行多次交易，多次交易之间不能交叉进行，可以进行多次交易。
+     * */
+    public int maxProfit2(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i]>prices[i-1]){
+                profit+=(prices[i]-prices[i-1]);
+            }
+        }
+        return profit;
+    }
+
+    public boolean checkPossibility(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        if (nums.length <= 2) {
+            return true;
+        }
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi && nums[hi - 1] <= nums[hi]) {
+            hi--;
+        }
+        while (lo < hi && nums[lo] <= nums[lo + 1]) {
+            lo++;
+        }
+        if (hi - lo > 1) {
+            return false;
+        }
+        // hi - lo <= 1 时：
+            // 除了nums[hi]其他都递增
+            return lo == 0 || hi == nums.length - 1
+                    // 除了边界，其他都递增
+                    || nums[lo - 1] <= nums[lo + 1]
+                    // 除了nums[lo]其他都递增
+                    || nums[hi - 1] <= nums[hi + 1];
+    }
+}
+
+
+/**
+ * @author ylqq
+ */
+public class Greedy {
+}
