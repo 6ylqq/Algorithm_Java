6ced39d3247432537b3f19dbd91c8795ccfffbfd
6ylqq <937192804@qq.com>
2020-12-02 10:25:16 +0800

    二叉查找，更新了查找区间这个题

--- LeetCode/src/main/java/BinarySearch.java
+++ LeetCode/src/main/java/BinarySearch.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @author ylqq
  *
@@ -47,6 +49,63 @@ public class BinarySearch {
                 return target;
             }
         }
+    }
+
+    /**
+     * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+     * */
+    public int[] searchRange(int[] nums, int target) {
+        if (nums.length==0){
+            return new int[]{-1,-1};
+        }
+
+        int first=findFirst(nums,target);
+        int end=findEnd(nums,target);
+        if(first == nums.length || nums[first] != target){
+            return new int[]{-1,-1};
+        }
+        return new int[]{first,end};
+    }
+
+    public int findFirst(int[] nums,int target){
+        int l=0,h=nums.length-1;
+        while (l<h){
+            /* 往下取 */
+            int m=l+(h-l)/2;
+            if (nums[m]<target){
+                l=m+1;
+            }else if (nums[m]==target){
+                h=m;
+            }else {
+                h=m-1;
+            }
+        }
+        return l;
+    }
+
+    public int findEnd(int[] nums,int target){
+        int l=0,h=nums.length-1;
+        while (l<h){
+            //往上取
+            int m=(l+h+1)/2;
+            if (nums[m]<target){
+                l=m+1;
+            }else if (nums[m]==target){
+                l=m;
+            }else {
+                h=m-1;
+            }
+        }
+        return l;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] nums={5,7,7,8,8,10};
+        int target=8;
+        BinarySearch binarySearch=new BinarySearch();
+        System.out.println(Arrays.toString(binarySearch.searchRange(nums, target)));
     }
 
 }
